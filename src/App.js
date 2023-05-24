dd875d59a64360f828d75904c371e122346b92c7
Saurabh Satish Mohite <saurabhmohite2214@gmail.com>
2023-05-24 10:06:38 +0530

    Compenent with param

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <EventDemo />
+      {/**EventDemo is name of function , attribute are parameter */}
+      <EventDemo Imageid="237" />
       <hr />
-      <EventDemo/>
+      <EventDemo Imageid="239" />
     </div>
   );
 }
 
-function EventDemo() {
+function EventDemo({ Imageid }) {
   let [counter, setCounter] = useState(100);
+
+  //statless function
+  let imageUrl = `http://picsum.photos/id/${Imageid}/200/200`;
 
   //Member func
   let likeMe = () => {
@@ -25,7 +29,7 @@ function EventDemo() {
 
   return (
     <div>
-      <img src="http://picsum.photos/id/237/300" alt="" />
+      <img src={imageUrl} alt="" />
       <h1>Like {counter}</h1>
       <input type="button" value="Like Me" onClick={likeMe} />
     </div>
