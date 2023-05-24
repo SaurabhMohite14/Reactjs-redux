e4e6da5662388953544714fdaed21fa2164bc696
Saurabh Satish Mohite <saurabhmohite2214@gmail.com>
2023-05-24 09:43:56 +0530

    function member type

--- src/App.js
+++ src/App.js
@@ -18,15 +18,42 @@ function EventDemo() {
   };
 
   //default covention of the param name is :: e,e1
+  // e very used=ful when working input element
   let likeMe1 = (e) => {
     console.log(e);
-    alert();
+    console.log(e.target);
   };
+
+  // custom param
+  let likeMe2 = (p1, p2) => {
+    console.log(p1);
+  };
+
+  let likeMe3 = (e, p1) => {
+    console.log(p1);
+    console.log(e);
+  };
+
   return (
     <div>
       <h1>Like {counter}</h1>
       <input type="button" value="Like Me" onClick={likeMe} />
-      <input type="button" value="Like Me" onClick={likeMe1} />
+      <input type="button" value="Like Me1" onClick={likeMe1} />
+      {/**For usr Defined param */}
+
+      <input
+        type="button"
+        value="Like Me2"
+        onClick={() => likeMe2("data", 100)}
+      />
+
+      {/**Default param + User Defined param */}
+
+      <input
+        type="button"
+        value="Like Me 3"
+        onClick={(e) => likeMe3(e, "data")}
+      />
     </div>
   );
 }
