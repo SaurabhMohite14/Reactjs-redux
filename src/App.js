9bd9b3c70b218bd106dc84d0f6cc50bd6afed503
Saurabh Satish Mohite <saurabhmohite2214@gmail.com>
2023-05-24 10:29:13 +0530

    take a input and change the image

--- src/App.js
+++ src/App.js
@@ -13,16 +13,30 @@ function ListDemo() {
   // let list = ["237", "239"];//stateless
   let [list, setList] = useState(["237"]); //statefull
 
+  let [imageInput, setImageInput] = useState("");
+
   let addNewImage = () => {
-    let newList = ["238", ...list];
+    // let newList = ["238", ...list];
+    let newList = [imageInput, ...list];
     setList(newList);
   };
+
+  let inputImageHandler = (e) => {
+    setImageInput(e.target.value);
+  };
+
   return (
     <div>
       <h1>List Demo</h1>
-
-      <input type="button" value="Add new Image" onClick={addNewImage} />
-
+      <div className="my-2">
+        <input
+          type="text"
+          placeholder="Enter Image id"
+          value={imageInput}
+          onChange={inputImageHandler}
+        />
+        <input type="button" value="Add new Image" onClick={addNewImage} />
+      </div>
       {list.map((item, index) => (
         <EventDemo key={index} Imageid={item} />
       ))}
