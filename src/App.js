9ce59984914c6aac1851dbc7b5ff81b36a01cc0b
Saurabh Satish Mohite <saurabhmohite2214@gmail.com>
2023-05-24 09:25:40 +0530

    ada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
-   <div>
-<h1>Hello world</h1>
+    <div>
+      <EventDemo />
+    </div>
+  );
+}
 
+function EventDemo() {
+  let [counter] = useState(100);
+
+  //Member func
+  let likeMe = () => {
+    alert();
+  };
+
+  //default covention of the param name is :: e,e1
+  let likeMe1 = (e) => {
+    console.log(e);
+    alert();
+  };
+  return (
+    <div>
+      <h1>Like {counter}</h1>
+      <input type="button" value="Like Me" onClick={likeMe} />
+      <input type="button" value="Like Me" onClick={likeMe1} />
     </div>
   );
 }
